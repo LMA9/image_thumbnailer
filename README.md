3e912515cb2d72c86d15f9dbb8186838276e917f
LeymanMA <leymanma@antereal.ru>
2021-08-20 12:17:35 +0700

    readme was added + sizes arg was added

---
+++ README.md
@@ -0,0 +1,25 @@
+# Image thumbnailer
+
+## Description
+
+Utility for image thumbnails creating in the loop.
+
+## Usage
+
+### Arguments
+
+Utility can be execute only with all arguments or without them.
+
+* Source folder - the folder where the source images will be placed. Default is `./images`
+* Target folder - the folder where the created thumbnails will be placed. Default is `./thumbnails`
+* Sizes - list of sizes devided by `,` char. Default is `240,480`
+
+### Example
+
+`image_thumbnailer`
+
+It runs loop that will convert all images from `./images` folder to `./thumbnails` folder(include folders for each size) and delete source images.
+
+`image_thumbnailer images/wallpapers my_thumbs 240,480,960`
+
+It runs loop that will convert all images from `./images/wallpapers` folder to `./my_thumbs` folder(include folders for each size) and delete source images.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::fs::{create_dir_all, read_dir, remove_file};
 use std::path::Path;
+use std::process::exit;
 use image::io::Reader;
 use image::DynamicImage;
 use std::thread;
 use std::time::Duration;
 use std::env;
 
+const HELP_STRING: &str = "
+Usage: `image_thumbnailer Optional['source_path', 'target_path', [thumbnails_sizes]]
+Example: `image_thumbnailer images/wallpapers thumbnails 240,480,960`";
 
 fn create_thumbnail(image: &DynamicImage, image_name: &str, size: u32, target_path: &String) {
     let thumbnails_path = format!("{}/{}", target_path, size);
@@ -23,12 +27,13 @@ fn create_thumbnail(image: &DynamicImage, image_name: &str, size: u32, target_pa
     }
 }
 
-fn create_thumbnails(image_path: &str, target_path: String) {
+fn create_thumbnails(image_path: &str, target_path: String, sizes: Vec<u32>) {
     let img = Reader::open(image_path).unwrap().decode().unwrap();
     let image_name = Path::new(image_path).file_name().unwrap().to_str().unwrap();
     
-    create_thumbnail(&img, image_name, 240, &target_path);
-    create_thumbnail(&img, image_name, 480, &target_path);
+    for size in sizes.iter() {
+        create_thumbnail(&img, image_name, *size, &target_path);
+    }
 
     remove_file(image_path).unwrap()
 }
@@ -37,15 +42,30 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let images_path;
     let target_path;
+    let sizes: Vec<u32>;
 
     match args.get(1) {
-        Some(source) => images_path = source.as_str(),
+        Some(arg) => {
+            let first_arg = arg.as_str();
+            if first_arg == "help" {
+                println!("{}", HELP_STRING);
+                exit(0);
+            }
+            images_path = first_arg
+        },
         None => images_path = "images"
     }
 
     match args.get(2) {
-        Some(source) => target_path = source.as_str(),
+        Some(arg) => target_path = arg.as_str(),
         None => target_path = "thumbnails"
+    }
+
+    match args.get(3) {
+        Some(sizes_string) => {
+            sizes = sizes_string.split(',').map(|s| s.parse().unwrap()).collect()
+        },
+        None => sizes = vec![240, 480]
     }
 
     println!("Thumbnailer was started.\nSource path: {}\nTarget path: {}", images_path, target_path);
@@ -54,10 +74,15 @@ fn main() {
         if Path::new(images_path).exists() {
             for res in read_dir(images_path).unwrap() {
                 let target = target_path.to_string();
+                let sizes = sizes.clone();
                 match res {
                     Ok(entry) => {
-                        if entry.path().is_file() {
-                            thread::spawn(move || { create_thumbnails(entry.path().to_str().unwrap(), target) });
+                        let entry_path = entry.path();
+                        if entry_path.is_file() {
+                            thread::spawn(move || {
+                                let file_path = entry_path.to_str().unwrap();
+                                create_thumbnails(file_path, target, sizes)
+                            });
                         }
                     }
                     Err(_e) => {}
